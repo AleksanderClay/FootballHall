c48f149b2a5b7b68e8bc4219530bec581d2f9f60
James Moreira <jamesmsjr@gmail.com>
2018-08-07 20:40:26 -0300

    Activity do Google Maps.

--- app/src/main/java/com/example/footballhall/footballhall/MainActivity.java
+++ app/src/main/java/com/example/footballhall/footballhall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.footballhall.footballhall;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,8 +84,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
+            startActivity(new Intent(getBaseContext(), MapsActivity.class));
+        //} else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
