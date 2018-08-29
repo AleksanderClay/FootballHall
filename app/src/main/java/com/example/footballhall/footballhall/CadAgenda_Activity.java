ef07527c1c4e3d73bf85f9f9b61cbbe14ec6d3b4
Marcones Nunes <marconesnunes16@gmail.com>
2018-08-29 20:23:36 -0300

    Mascara retirada

--- app/src/main/java/com/example/footballhall/footballhall/CadAgenda_Activity.java
+++ app/src/main/java/com/example/footballhall/footballhall/CadAgenda_Activity.java
@@ -77,6 +77,7 @@ public class CadAgenda_Activity extends AppCompatActivity {
 
                 editNome.setText(cliente.nome);
                 editTel.setText(cliente.telefone);
+                editTel.addTextChangedListener(MaskEditUtil.mask(editTel, MaskEditUtil.FORMAT_FONE));
                 spinner_Arena.setOnItemSelectedListener(new CustomOnItemSelectedListener());
                 editHora.setOnItemSelectedListener(new CustomOnItemSelectedListener());
 
@@ -91,6 +92,7 @@ public class CadAgenda_Activity extends AppCompatActivity {
 
             editNome.setText(cliente.nome);
             editTel.setText(cliente.telefone);
+            editTel.addTextChangedListener(MaskEditUtil.mask(editTel, MaskEditUtil.FORMAT_FONE));
             spinner_Arena.setOnItemSelectedListener(new CustomOnItemSelectedListener());
             editHora.setOnItemSelectedListener(new CustomOnItemSelectedListener());;
         } catch (Exception e){
